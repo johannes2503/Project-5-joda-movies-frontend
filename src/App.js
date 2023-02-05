85a7a56f659d558b3e4896a73c7117cc66e9cf82
Cryo79 <johannes2503@gmail.com>
2023-02-05 16:52:20 +0000

    Working on login and register modals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import styles from "./styles/App.module.css";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container></Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 const NavBar = () => {
+  const [LoginmodalShow, setLoginModalShow] = React.useState(false);
+  const [RegistermodalShow, setRegisterModalShow] = React.useState(false);
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container className="d-flex">
@@ -13,17 +17,79 @@ const NavBar = () => {
             <Nav.Link className="text-light">
               <i className="fas fa-home me-2"></i>HOME
             </Nav.Link>
-            <Nav.Link className="text-light">
+            <Nav.Link
+              className="text-light"
+              onClick={() => setLoginModalShow(true)}
+            >
               <i className="fas fa-sign-in-alt me-2"></i>LOGIN
             </Nav.Link>
-            <Nav.Link className="text-light">
+            <Nav.Link
+              className="text-light"
+              onClick={() => setRegisterModalShow(true)}
+            >
               <i className="fas fa-user-plus me-2"></i>SIGN UP
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        <LoginModal
+          show={LoginmodalShow}
+          onHide={() => setLoginModalShow(false)}
+        />
+        <RegisterModal
+          show={RegistermodalShow}
+          onHide={() => setRegisterModalShow(false)}
+        />
       </Container>
     </Navbar>
   );
 };
+
+function LoginModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>Test</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function RegisterModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>Test 3</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 export default NavBar;

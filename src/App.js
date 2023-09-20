e1bcdeb6318f471e9111c4227381e01f2dbbdaa7
Cryo79 <johannes2503@gmail.com>
2023-09-20 13:03:16 +0000

    Starting again after crash

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import MovieDetails from "./components/movie-details";
-import MovieList from "./components/movie-list";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [movies, setMovie] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/movies/", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Token 1eb87d911347dd18f0d0efc745897d57146e77e7",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((resp) => setMovie(resp))
-      .catch((error) => console.log(error));
-  }, []);
-
-  const movieClicked = (movie) => {
-    setSelectedMovie(movie);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Joda Movies</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <div className="layout">
-        <MovieList movies={movies} movieClicked={movieClicked} />
-        <MovieDetails movie={selectedMovie} />
-      </div>
     </div>
   );
 }

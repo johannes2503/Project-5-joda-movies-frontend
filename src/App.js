f3b156ace9c2378ad51de2a52ea539000a8e9536
Cryo79 <johannes2503@gmail.com>
2023-09-20 11:44:18 +0000

    Setting up communication between backend and front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import MovieDetails from "./components/movie-details";
+import MovieList from "./components/movie-list";
 
 function App() {
+  const [movies, setMovie] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token 1eb87d911347dd18f0d0efc745897d57146e77e7",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => setMovie(resp))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const movieClicked = (movie) => {
+    setSelectedMovie(movie);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Joda Movies</h1>
       </header>
+      <div className="layout">
+        <MovieList movies={movies} movieClicked={movieClicked} />
+        <MovieDetails movie={selectedMovie} />
+      </div>
     </div>
   );
 }
---
+++ src/components/movie-list.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function MovieList(props) {
+  const movieClicked = (movie) => (evt) => {
+    props.movieClicked(movie);
+  };
+
+  return (
+    <div>
+      <div>
+        {props.movies &&
+          props.movies.map((movie) => {
+            return (
+              <div key={movie.id}>
+                <h2 onClick={movieClicked(movie)}>{movie.title}</h2>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default MovieList;
